24c4e28b2351dd6062008790b5bd18a07ff4208f
Arlene George <aemg@live.com>
2019-08-11 22:31:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   lists_in_ruby = ["hash"]
-  yummy_fruit << "arrays!"
+  lists_in_ruby << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
