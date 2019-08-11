2d14ae17c70e0647f5c8cce90d507cf85787f938
Arlene George <aemg@live.com>
2019-08-11 22:34:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   ruby_stuff = ["methods", "hashes", "arrays!"]
-  ruby_stuff.shift("arrays!")
+  arrays! = ruby_stuff.shift
 end
 
 def remove_element_from_start_of_array(array)
