847e581679472b6ce6bcabd803ceac38c7d82b13
Arlene George <aemg@live.com>
2019-08-11 22:20:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  green_veggies = ("broccoli", "iceberg")
+  green_veggies = ["broccoli", "iceberg"]
   green_veggies.unshift("spinach")
 end
 
