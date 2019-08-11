486944c2ab1cccfb829ae32b0ed6e2f8e9ed80a5
Arlene George <aemg@live.com>
2019-08-11 22:15:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   yummy_fruit = ["peach", "Orange"]
-  yummy_fruit << "strawberry"
+  yummy_fruit << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
